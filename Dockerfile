09e8b64f288266c8c519648f9024a4b30d12ea80
wangyunshu <13041202543@163.com>
2023-05-25 15:31:39 +0800

    测试环境变量

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,4 @@
 FROM openjdk:17
-ENV active=local
 ADD target/ddns.jar /application/ddns.jar
-ENTRYPOINT ["java", "-jar", "/application/Apollo.jar"]
+ENTRYPOINT ["java", "-jar", "/application/ddns.jar"]
 EXPOSE 8080
--- src/main/java/vip/mrtree/ddns/schedule/Ali.java
+++ src/main/java/vip/mrtree/ddns/schedule/Ali.java
@@ -1,12 +1,29 @@
 package vip.mrtree.ddns.schedule;
 
+import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Slf4j
 @Component
 public class Ali extends AbsSchedule{
+    private final static Set<String> ENV_KEY_SET = new HashSet<>();
+
+    static {
+        ENV_KEY_SET.add("ALI_DOMAIN_NAME_SERVER_REGION");
+        ENV_KEY_SET.add("ALI_ACCESS_KEY");
+        ENV_KEY_SET.add("ALI_ACCESS_KEY_SECRET");
+    }
+
+    @PostConstruct
+    public void init() {
+        ENV_KEY_SET.forEach(key -> log.info("{} = {}", key, System.getenv(key)));
+    }
+
     @Scheduled(fixedDelay = 60 * 1000)
     public void ali() {
         scheduleTemplate(this.getClass().getSimpleName());
